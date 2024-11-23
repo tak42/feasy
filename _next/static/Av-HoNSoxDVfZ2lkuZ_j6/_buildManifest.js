fdb633d507dbbb84252b7a00528535255d648bbf
tak42 <tak42@users.noreply.github.com>
2024-11-23 04:26:03 +0000

    deploy: a6dc2205d11f75ab20c1e341c1dd3653436fdd2a

---
+++ _next/static/Av-HoNSoxDVfZ2lkuZ_j6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/351ed3fed335ab80.css","static/chunks/pages/index-525b4c8a0bdc51b0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
