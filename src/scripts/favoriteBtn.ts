1e8a63704fdfddbf7597c6574ff0b4329bb2009b
tak42 <1206ookubo@gmail.com>
2024-11-29 07:43:11 +0900

    feat: add shareForm

--- src/scripts/favoriteBtn.ts
+++ src/scripts/favoriteBtn.ts
@@ -1,13 +1,30 @@
+type PostActions = 'hide' | 'share';
+
+type PostContents = {
+  kind: string;
+  value: string;
+}[];
+
+type PostData = {
+  action: PostActions;
+  contents: PostContents;
+};
+
+type PostFunc = {
+  action: PostActions;
+  func: (contents: PostContents) => void;
+};
+
 type Styles = { property: keyof CSSStyleDeclaration; value: string }[];
 
-const localhostUrl = 'http://localhost:3000';
+const LOCALHOST_URL = 'http://localhost:3000';
 
 const btnStyle: Styles = [
-  { property: 'width', value: '100px' },
+  { property: 'width', value: '150px' },
   { property: 'height', value: '50px' },
   { property: 'color', value: 'red' },
   { property: 'position', value: 'absolute' },
-  { property: 'top', value: '100px' },
+  { property: 'top', value: '20%' },
   { property: 'left', value: '100px' },
 ];
 
@@ -18,6 +35,7 @@ const containerStyle: Styles = [
   { property: 'top', value: '50%' },
   { property: 'left', value: '50%' },
   { property: 'transform', value: 'translate(-50%, -50%)' },
+  { property: 'backgroundColor', value: 'white' },
 ];
 
 const iframeStyle: Styles = [
@@ -33,7 +51,7 @@ const setStyle = (htmlElm: HTMLElement, styles: Styles) => {
 
 const showIframe = () => {
   const iframe = document.createElement('iframe');
-  iframe.src = localhostUrl;
+  iframe.src = LOCALHOST_URL;
   iframe.sandbox.value = 'allow-scripts allow-same-origin';
   setStyle(iframe, iframeStyle);
   container.appendChild(iframe);
@@ -44,8 +62,47 @@ const hideIframe = () => {
   if (iframe) container.removeChild(iframe);
 };
 
+const targetInputs = [
+  {
+    name: 'q1',
+    kind: 'name',
+  },
+  {
+    name: 'q2',
+    kind: 'email',
+  },
+  {
+    name: 'q5',
+    kind: 'old',
+  },
+];
+
+const searchTargetInputs = () => {
+  return Array.from(document.getElementsByTagName('input')).filter((elm) =>
+    targetInputs.some((val) => val.name === elm.name)
+  );
+};
+
+const shareForm = (contents: PostContents) => {
+  const inputElms: HTMLInputElement[] = searchTargetInputs();
+  if (!inputElms.length) return false;
+  targetInputs.forEach((input) => {
+    const content = contents.find(({ kind }) => kind === input.kind);
+    inputElms.filter(({ name }) => name === input.name)[0].value = content ? content.value : '';
+  });
+  hideIframe();
+};
+
+const iframePostActions: PostFunc[] = [
+  { action: 'hide', func: hideIframe },
+  { action: 'share', func: shareForm },
+];
+
 window.addEventListener('message', (event) => {
-  if (event.origin === localhostUrl) hideIframe();
+  if (event.origin !== LOCALHOST_URL) return false;
+  const postData: PostData = event.data;
+  const actionFunc = iframePostActions.filter((val) => val.action === postData.action);
+  actionFunc[0].func(postData.contents);
 });
 
 const btn = document.createElement('button');
