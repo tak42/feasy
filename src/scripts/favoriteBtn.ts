59e266b6aca75321215aa86b90350b2d324ca045
tak42 <1206ookubo@gmail.com>
2024-12-17 14:00:56 +0900

    refactor: FavoriteBtn.ts is now a module.

--- src/scripts/favoriteBtn.ts
+++ src/scripts/favoriteBtn.ts
@@ -1,76 +1,7 @@
-type PostActions = 'hide' | 'share';
-
-type PostContent = {
-  name: string;
-  email: string;
-  old: string;
-};
-
-type PostData = {
-  action: PostActions;
-  content: PostContent;
-};
-
-type PostFunc = {
-  [key in PostActions]: (content: PostContent) => void;
-};
-
-type Styles = { property: keyof CSSStyleDeclaration; value: string }[];
-
-const LOCALHOST_URL = 'http://localhost:3000';
-
-const btnStyle: Styles = [
-  { property: 'width', value: '160px' },
-  { property: 'height', value: '56px' },
-  { property: 'color', value: 'red' },
-  { property: 'position', value: 'absolute' },
-  { property: 'top', value: '160px' },
-  { property: 'left', value: '80px' },
-];
-
-const containerStyle: Styles = [
-  { property: 'height', value: '50%' },
-  { property: 'width', value: '50%' },
-  { property: 'position', value: 'absolute' },
-  { property: 'top', value: '50%' },
-  { property: 'left', value: '50%' },
-  { property: 'transform', value: 'translate(-50%, -50%)' },
-];
-
-const containerHideStyle: Styles = [
-  { property: 'height', value: '0' },
-  { property: 'width', value: '0' },
-];
-
-const iframeStyle: Styles = [
-  { property: 'height', value: '100%' },
-  { property: 'width', value: '100%' },
-  { property: 'background', value: 'white' },
-];
-
-const setStyle = (htmlElm: HTMLElement, styles: Styles) => {
-  styles.forEach((val) => {
-    htmlElm.style.setProperty(String(val.property), val.value);
-  });
-};
-
-const showIframe = () => {
-  setStyle(container, containerStyle);
-
-  if (container.querySelector('iframe')) return;
-
-  const iframe = document.createElement('iframe');
-
-  iframe.src = LOCALHOST_URL;
-  iframe.sandbox.value = 'allow-scripts allow-same-origin';
-  setStyle(iframe, iframeStyle);
-
-  container.appendChild(iframe);
-};
-
-const hideIframe = () => {
-  setStyle(container, containerHideStyle);
-};
+import { LOCALHOST_URL } from '../common';
+import { hideIframe, showIframe } from '../hooks/iframe';
+import { btnStyle, setStyle } from '../hooks/styles';
+import type { PostContent, PostData, PostFunc } from '../types/Post.type';
 
 const combineIdentifiers: { id: string; kind: keyof PostContent }[] = [
   {
@@ -98,11 +29,15 @@ const shareForm = (content: PostContent) => {
     elm.value = content[val.kind];
   });
 
-  hideIframe();
+  hideIframe(container);
+};
+
+const hideForm = () => {
+  hideIframe(container);
 };
 
 const iframePostActions: PostFunc = {
-  hide: hideIframe,
+  hide: hideForm,
   share: shareForm,
 };
 
@@ -119,7 +54,7 @@ const container = document.createElement('div');
 
 btn.innerText = 'iframe 表示';
 setStyle(btn, btnStyle);
-btn.addEventListener('click', showIframe);
+btn.addEventListener('click', () => showIframe(container));
 
 document.body.appendChild(btn);
 document.body.appendChild(container);
