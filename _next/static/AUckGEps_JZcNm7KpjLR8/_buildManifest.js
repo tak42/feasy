788922aee451b46ad12eac95553d93c07c1e5cd5
tak42 <tak42@users.noreply.github.com>
2025-01-14 13:46:24 +0000

    deploy: c47fdaba84a669c83742a792853f791257a62159

--- _next/static/AUckGEps_JZcNm7KpjLR8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/ee7dc042cde6f682.css","static/chunks/pages/index-5a81b0897878c675.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
