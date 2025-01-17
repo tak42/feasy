aab1ab566b8ceae7509a1133c979679cde834c04
tak42 <nengdengguihui@gmail.com>
2025-01-17 21:41:04 +0900

    refactor: delete comment and add export

--- src/types/Post.type.ts
+++ src/types/Post.type.ts
@@ -1,12 +1,11 @@
-type PostActions = 'hide' | 'share' | 'check';
+export type PostActions = 'hide' | 'share' | 'check';
 
-type SharedCombination = { id: string; value: string };
+export type SharedCombination = { id: string; value: string };
 
 export type CombinationOrigins = {
   [key: string]: SharedCombination[];
 };
 
-// PostDataのコンテンツはオリジンのキーから抽出した組み合わせを格納する
 export type PostData = {
   action: PostActions;
   content: SharedCombination[];
