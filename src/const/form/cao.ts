bda2f70015e53b3e5767ad4de3040a2035a72399
tak42 <nengdengguihui@gmail.com>
2025-01-26 23:35:51 +0900

    refactor: create config value

---
+++ src/const/form/cao.ts
@@ -0,0 +1,8 @@
+export type valueType = {
+  name: string;
+  email: string;
+  tel: string;
+  old: number;
+  gender: string;
+  business: string;
+};
---
+++ src/const/index.ts
@@ -0,0 +1,9 @@
+export const allowedOrigins = ['https://form.cao.go.jp', 'https://hoge.co.jp'];
+
+export const stringValueTypes = ['firstName', 'famiryName', 'email', 'tel', 'business'];
+
+export const numberValueTypes = ['old'];
+
+export const specialValueTypes = ['gender'];
+
+export const gender = ['男', '女'];
