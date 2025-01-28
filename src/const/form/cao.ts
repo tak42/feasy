9bcd46c016154be21daee06e76aff59cc5f9b2f2
tak42 <nengdengguihui@gmail.com>
2025-01-29 00:11:15 +0900

    refactor: add cao.ts types

--- src/const/form/cao.ts
+++ src/const/form/cao.ts
@@ -1,8 +1,40 @@
-export type valueType = {
-  name: string;
-  email: string;
-  tel: string;
-  old: number;
-  gender: string;
-  business: string;
+import type { TransferValue } from '../../types';
+
+// type Gender = ['男', '女'];
+
+// SupportedInputValuesから拡張させないと、好き勝手に指定できてしまう
+type ExistsInputValues = ['name', 'email', 'tel', 'old', 'gender', 'business'];
+
+export type CaoDto = {
+  [key in ExistsInputValues[number]]: TransferValue;
+};
+
+// sourceValueに当て込むための引数の型定義をする
+export const createTrasferDto = (): CaoDto => {
+  return {
+    name: {
+      SpecifiedId: 'q1',
+      sourceValue: '',
+    },
+    email: {
+      SpecifiedId: 'q1',
+      sourceValue: '',
+    },
+    tel: {
+      SpecifiedId: 'q1',
+      sourceValue: '',
+    },
+    old: {
+      SpecifiedId: 'q1',
+      sourceValue: '',
+    },
+    gender: {
+      SpecifiedId: 'q1',
+      sourceValue: '',
+    },
+    business: {
+      SpecifiedId: 'q1',
+      sourceValue: '',
+    },
+  };
 };
