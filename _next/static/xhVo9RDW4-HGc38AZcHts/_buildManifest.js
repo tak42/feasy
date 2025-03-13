c9f0a94de5b3ea6e0340860051776e4139945df6
tak42 <tak42@users.noreply.github.com>
2025-03-13 00:09:51 +0000

    deploy: 9f420cc2529b89c8913057bddbb2b4f77ae8a12b

---
+++ _next/static/xhVo9RDW4-HGc38AZcHts/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/5b63cfca7d573e36.css","static/chunks/pages/index-fe5ef91deb38e8b9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
